3b7e6db8592135f8c45ef90ae8119ce7c5fcb8d0
thaynaraandradde <00001108085581sp@al.educacao.sp.gov.br>
2024-12-06 15:27:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Historia', 
+    'Qual foi o país que venceu a Guerra Fria?', 
+    'O pais que venceu a guerra fria foi os "estados unidos" no ano de 1991'
+)
+
+criaCartao(
+    'Ciências', 
+    'Qual é a fórmula química da água?', 
+    'A fórmula química da água é H2O'
+)
+
+criaCartao(
+    'Matemática', 
+    'Qual é o valor de π (pi) aproximadamente?', 
+    'O valor de π (pi) é aproximadamente 3,14'
+)
+
+criaCartao(
+    'Atualidade', 
+    'Quem ganhou as eleições dos Estados Unidos?', 
+    'Quem anhou as eleições dos estados unidos foi Donald Trump'
+)
+
+criaCartao(
+    'Educação Fisica', 
+    'Quantas Copas do Mundo de Futebol o Brasil já venceu?', 
+    'O Brasil já venceu 5 Copas do Mundo de Futebol'
+)
+
+criaCartao(
+    'Química', 
+    'Qual é o elemento químico com símbolo "C"?', 
+    'O elemento químico com símbolo "C" é o Carbono'
+)
+
+criaCartao(
+    'Física', 
+    'Quem formulou a Lei da Gravitação Universal?', 
+    'A Lei da Gravitação Universal foi formulada por Isaac Newton'
+)
+
+criaCartao(
+    'Programação', 
+    'O que é "HTML"?', 
+    'é o código que você usa para estruturar uma página web e seu conteúdo'
+)
+
+criaCartao(
+    'Língua Portuguesa', 
+    'Quem  escreveu o gibi a turma do Chico Bento?', 
+    'Quem escreveu a turma de Chico Bento foi "Mauricio de Souza"'
+)
+
+criaCartao(
+    'Tecnologia', 
+    'O que é inteligência artificial?', 
+    'Inteligência artificial é a simulação da inteligência humana por máquinas, especialmente por sistemas de computador'
+)
+
+criaCartao(
+    'Artes', 
+    'Quem pintou o quadro ㅤㅤㅤㅤㅤㅤㅤㅤ"A Monalisa"?', 
+    '"A Monalisa" foi pintada por Leonardo da Vinci'
+)
+
+criaCartao(
+    'Geografia', 
+    'Qual é o planeta mais próximo da Terra?', 
+    'O planeta mais próximo da Terra é Mercúrio'
+)
